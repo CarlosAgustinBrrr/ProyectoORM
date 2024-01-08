ce0960c779f05ae24f85ea37dd4a4026500488ed
CarlosAgustinBrrr <agustin.gacar18@zaragoza.salesianos.edu>
2024-01-08 16:50:12 +0100

    Cambios Carlos

--- djangoConcesionario/concesionario/views.py
+++ djangoConcesionario/concesionario/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Coche  # Importa tu modelo Coche
 
-# Create your views here.
+def lista_coches(request):
+    # Obtiene todos los coches de la base de datos
+    coches = Coche.objects.all()
+
+    # Pasa la lista de coches al template
+    context = {'coches': coches}
+
+    # Renderiza el template y devuelve la respuesta
+    return render(request, 'concesionario/lista_coches.html', context)
