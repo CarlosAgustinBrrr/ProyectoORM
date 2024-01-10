94fcbbb2c2b59eff52eb904e956b5b688761b270
Marcos Burgos <burgos.lomar18@zaragoza.salesianos.edu>
2024-01-10 20:23:45 +0100

    Cambios formulario

--- djangoConcesionario/concesionario/views.py
+++ djangoConcesionario/concesionario/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from .models import Marca
-from .models import Vehiculo
-from .models import Modelo
-
+from .models import Marca, Modelo, Vehiculo
+from .forms import VehiculoForm
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+def crearVehiculo(request):
+    if request.method == 'POST':
+        vehiculo_form = VehiculoForm(request.POST)
+        if vehiculo_form.is_valid():
+            vehiculo_form.save()
+            return render(request, "index.html")
+    else:
+        vehiculo_form = VehiculoForm()
+    return render(request, "crearVehiculo.html", {'vehiculo_form':vehiculo_form})
